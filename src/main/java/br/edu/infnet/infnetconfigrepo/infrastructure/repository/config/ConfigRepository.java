dcec818fbb230f1c008d7fbf7f35920af86280f0
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-12-10 15:07:05 -0300

    Refactored EurekaClient and ConfigServer to Application

--- src/main/java/br/edu/infnet/infnetconfigrepo/infrastructure/repository/config/ConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.infnet.infnetconfigrepo.infrastructure.repository.config;
-
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigServer
-public class ConfigRepository {
-}
